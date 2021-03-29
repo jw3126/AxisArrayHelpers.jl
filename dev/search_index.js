4bfdfaa72fbdfe0b1a33dda45a9c125fc8282625
Documenter.jl <documenter@juliadocs.github.io>
2021-03-29 05:59:08 +0000

    build based on 8411f14

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AxisArrayHelpers","category":"page"},{"location":"#AxisArrayHelpers","page":"Home","title":"AxisArrayHelpers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AxisArrayHelpers]","category":"page"},{"location":"#AxisArrayHelpers.common_ground-Tuple{Any}","page":"Home","title":"AxisArrayHelpers.common_ground","text":"new_axis_arrays = common_ground(axis_arrays)\n\nTake a collection of axis array like objects, with possibly distinct but overlapping axes and shrink them such that their axes coincide.\n\n\n\n\n\n","category":"method"}]
+}
